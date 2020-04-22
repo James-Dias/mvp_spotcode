a6e123de25368815dfeb6407a9278313bb5ba95e
James Dias <jamesdias0@gmail.com>
2020-04-21 22:41:56 -0300

    Add top menu, pages home and discovery

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -3,12 +3,15 @@ import 'react-bulma-components/dist/react-bulma-components.min.css';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom'
 import 'app.scss'
+import Menu from './components/common/menu'
+import { useLocation } from 'react-router-dom'
 
 
 const App = () => {
   return(
     <Fragment>
       <BrowserRouter>
+        <Menu/>
         <Routes/>
       </BrowserRouter>
     </Fragment>
